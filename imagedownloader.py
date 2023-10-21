1a6691cec57dcbdcf4572a98cd1c31a9a96a480a
Cody Simon <97569855+BobTheChickenPlzHelp@users.noreply.github.com>
2023-10-21 00:45:27 -0400

    Update imagedownloader.py
    Made it better and runs over and over again and creates random image names

--- imagedownloader.py
+++ imagedownloader.py
@@ -7,8 +7,10 @@ import shutil # save img locally
 import os # basic commands
 from PIL import Image # Imports PIL module  
 timer = 30 #
+repeat = 10
 print("Import Complete")
-file_name = input('Save image as (string):') # prompt user for file name THIS IS TEMPERARY
+
+#file_name = input('Save image as (string):') # prompt user for file name THIS IS TEMPERARY
 # gunna make "file_name" = "gaysex-[random int 1-100000]"
 # this will make the program be able to create(download) multiplle images of itself :)
 url = "https://steamuserimages-a.akamaihd.net/ugc/1824532753041605480/9A6C19AD80E2DBA6ECC00682A6FE692ED178247E/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
@@ -17,6 +19,8 @@ res = requests.get(url, stream = True)
 print("Startup Complete")
 def imagedownloader(imageurl):
     print("you called imagedownloader")
+    imagenumber = (random.randint(1, 100000))
+    file_name = "testimage-" + str(imagenumber) + ".jpg"
     
     #--------------
     # WILL ADD CODE LATER FOR NOW THIS STAYS EMPTY CUZ IM FUCKING LAZY
@@ -28,13 +32,21 @@ def imagedownloader(imageurl):
         print('Image Couldn\'t be retrieved')
     #--------------
     
+    # open method used to open different extension image file 
+    print("pretest")
+    filelocation = "D:\.D.Downloads\SomeDumbassProgram" + file_name
+    #im = Image.open(filelocation) 
+    im = Image.open(r"D:\.D.Downloads\SomeDumbassProgram\imagetest.jpg")
+    im.show()  
+
+    
+    #--------------
+    
+    
     #r = requests.get('https://steamuserimages-a.akamaihd.net/ugc/1824532753041605480/9A6C19AD80E2DBA6ECC00682A6FE692ED178247E/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false')
     #'Requests:' in r.text
-imagedownloader(url)
+x = 1
+for x in range(repeat):
+    x = x + 1
+    imagedownloader(url)
 
-# open method used to open different extension image file 
-print("pretest")
-filelocation = "D:\.D.Downloads\SomeDumbassProgram" + file_name
-#im = Image.open(filelocation) 
-im = Image.open(r"D:\.D.Downloads\SomeDumbassProgram\imagetest.jpg")
-im.show()  
